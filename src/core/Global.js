17b95e57c7e5d18717a9243c212cde0a0831aa2f
Callum <callumnewsome100@gmail.com>
2021-09-15 21:55:41 +0100

    Base Global core

--- src/core/Global.js
+++ src/core/Global.js
@@ -1,9 +1,18 @@
 const { Client } = require('discord.js');
+const path = require('path');
+const fs = require('fs');
+
 const config = require('./config');
+const db = require('./database')
+const utils = require('./utils');
+const logger = require('./logger');
 
 class Global {
     constructor() {
         this.isReady = false;
+
+        process.on('unhandledRejection', this.handleRejection.bind(this));
+		process.on('uncaughtException', this.handleException.bind(this));
     }
 
     get client() {
@@ -14,19 +23,75 @@ class Global {
         return config;
     }
 
+    get db() {
+        return db;
+    }
+
+    get models() {
+        return db.models;
+    }
+
+    get utils() {
+        return utils;
+    }
+
+    get logger() {
+        return logger;
+    }
+
     async init(options) {
         options = options || {};
         
-        this.options = Object.assign(config.clientOptions, options);
+        this.options = Object.assign(config.clientOptions || {}, options);
 
         this._client = new Client(this.options);
 
+        const DJSLogger = logger.get('D.JS');
+
+        this.client.on('error', err => DJSLogger.error(err));
+		this.client.on('warn', err => DJSLogger.error(err));
+		this.client.on('debug', msg => {
+			if (typeof msg === 'string') {
+				msg = msg.replace(config.token, 'token');
+			}
+
+			DJSLogger.debug(msg);
+		});
+
         this.client.once('ready', this.ready.bind(this));
+
+        this.login();
     }
 
     ready() {
         this.isReady = true;
+
+        logger.get('Client').info(`${this.client.user.username} is online with ${this.client.guilds.cache.size} guilds & ${this.client.users.cache.size} users`, 'Ready')
     }
+
+    login() {
+        this.client.login(this.config.token);
+
+        return true;
+    }
+
+    handleRejection(reason, p) {
+		try {
+			console.error('Unhandled rejection at: Promise', p, 'reason:', reason);
+		} catch (err) {
+			console.error(reason);
+		}
+	}
+
+    handleException(err) {
+		const time = (new Date()).toISOString();
+		let report = `Crash Report ${time}:\n\n${err.stack}`;
+
+		report += `\n\nClient Options: ${JSON.stringify(this.config.clientOptions)}`;
+
+		const file = path.join(__dirname, `crashreport_${time}.txt`);
+		fs.writeFileSync(file, report);
+	}
 }
 
 module.exports = Global;
