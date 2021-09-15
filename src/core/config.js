f18a89f6d034fbf9eef86b4a5082ba5565ffcbf4
Callum <callumnewsome100@gmail.com>
2021-09-15 21:55:04 +0100

    Base config

--- src/core/config.js
+++ src/core/config.js
@@ -1,1 +1,47 @@
-//
+const pkg = require('../../package.json');
+const { Intents } = require('discord.js');
+
+const config = {
+    clientOptions: {
+        shards: 'auto',
+        shardCount: 1,
+        userAgentSuffix: [ 'Global', `v${pkg.version}`],
+        presence: {
+            status: 'online',
+            activities: [
+                { name: `Global bot v${pkg.version}`, type: 'WATCHING' }
+            ]
+        },
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_BANS,
+            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+            Intents.FLAGS.GUILD_INTEGRATIONS,
+            Intents.FLAGS.GUILD_WEBHOOKS,
+            Intents.FLAGS.GUILD_INVITES,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILD_PRESENCES,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_MESSAGE_TYPING,
+            Intents.FLAGS.DIRECT_MESSAGES,
+            Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+            Intents.FLAGS.DIRECT_MESSAGE_TYPING
+        ]
+    },
+    token: process.env.BOT_TOKEN,
+    logger: {
+        logLevel: 'INFO'
+    },
+    db: {
+        database: process.env.DB_DATABASE,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host:     process.env.DB_HOST,
+        dialect:  process.env.DB_DIALECT,
+        port:     process.env.DB_PORT
+    }
+};
+
+module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Global = require('./core/Global');
 const options = {};
 
