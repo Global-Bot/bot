38bdca78c1844c9f61daae49a161ef009317176e
Callum <callumnewsome100@gmail.com>
2021-09-15 21:48:36 +0100

    DataBase

--- src/core/database.js
+++ src/core/database.js
@@ -1,1 +1,9 @@
-//
+const globalDatabase = require('@global-bot/database');
+const config = require('./config');
+const logger = require('./logger');
+
+const db = new globalDatabase(Object.assign(config, {
+    logger: logger.get('DataBase')
+}));
+
+module.exports = db;
