4c89c59557f09b7beb9d84aebcb49645819f81ec
Callum <callumnewsome100@gmail.com>
2021-09-18 15:07:36 +0100

    messageCreate listener

---
+++ src/events/messageCreate.js
@@ -0,0 +1,11 @@
+
+module.exports = function messageCreate(dispatcher, message) {
+	if (!dispatcher.global.isReady || !message) return Promise.reject();
+    
+    return new Promise((resolve) => {
+		resolve({
+            message,
+            guild: message.guild
+        })
+	});
+}
