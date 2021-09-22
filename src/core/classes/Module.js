33fb77d10a9ad1eb9847efaeb774dd9d49db21a1
Callum <callumnewsome100@gmail.com>
2021-09-22 13:19:52 +0100

    Job scheduler for Modules

--- src/core/classes/Module.js
+++ src/core/classes/Module.js
@@ -1,9 +1,17 @@
 const Base = require('./Base');
+const schedule = require('node-schedule');
 
 class Module extends Base {
     constructor(global) {
         super(global);
+
+        this._jobs = [];
     }
+
+    schedule(interval, task) {
+        const job = schedule.scheduleJob(interval, task)
+		this._jobs.push(job);
+	}
 
 }
 
