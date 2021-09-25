d6d82cd33744e2d041cc0d4fea7fff125a43dd90
Callum <callumnewsome100@gmail.com>
2021-09-26 00:41:02 +0100

    Add data param to interactionCreate listener

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,9 +1,16 @@
 module.exports = async function interactionCreate(dispatcher, interaction) {
     if (!dispatcher.global.isReady || !interaction) return Promise.reject();
 
+    let data = null;
+    const hasData = await dispatcher.global.hasData(interaction.customId);
+    if (hasData) {
+        data = await dispatcher.global.getData(interaction.customId);
+    }
+
     return new Promise((resolve) => {
         resolve({
             interaction,
+            data,
             isAdmin: dispatcher.global.permissions.isAdmin(interaction.user)
         })
     });
