3a4d3e804e6a02d31594b9f91bb23e187fbd51bf
Callum <callumnewsome100@gmail.com>
2021-09-25 16:28:46 +0100

    Fix problem causing indentation errors on modules

--- src/core/classes/Module.js
+++ src/core/classes/Module.js
@@ -4,9 +4,9 @@ const schedule = require('node-schedule');
 class Module extends Base {
     constructor(global) {
         super(global);
-
+        
         this._jobs = [];
-
+        
         this._moduleProps = [
             { name: 'module',      type: 'string',  optional: false },
             { name: 'description', type: 'string',  optional: false },
@@ -14,46 +14,34 @@ class Module extends Base {
             { name: 'enabled',     type: 'boolean', optional: false },
         ];
     }
-
+    
     schedule(interval, task) {
         const job = schedule.scheduleJob(interval, task)
-		this._jobs.push(job);
-	}
-
+        this._jobs.push(job);
+    }
+    
     registerHandler(event, handler) {
         return this.global.dispatcher.registerHandler(event, handler.bind(this))
     }
-
+    
     _init(global) {
         if (this.init) {
             this.init(global);
         }
     }
-
+    
     ensure() {    
         function ensureError(err, prop, exists, type, received) {
-			return JSON.stringify({ err, prop, exists, type, received });
-		}
-
+            return JSON.stringify({ err, prop, exists, type, received });
+        }
+        
         for (const { name, type, optional } of this._moduleProps) {
             if (!Object.hasOwnProperty.call(this, name) && typeof this[name] != type && !optional) {
-                throw new Error(ensureError(
-                    `Module "${this.constructor.name}": Required prop "${name}" does not exist`,
-                    name,
-                    false,
-                    type,
-                    typeof this[name]
-                ));
+                throw new Error(ensureError(`Module "${this.constructor.name}": Required prop "${name}" does not exist`, name, false, type, typeof this[name]));
             }
             
             if (typeof this[name] != type && !(optional && typeof this[name] == 'undefined')) {
-                throw new Error(ensureError(
-                    `Module "${this.constructor.name}": Required prop "${name}" is the wrong type, should be "${type}", received: "${typeof this[name]}"`,
-                    name,
-                    true,
-                    type,
-                    typeof this[name]
-                ));
+                throw new Error(ensureError(`Module "${this.constructor.name}": Required prop "${name}" is the wrong type, should be "${type}", received: "${typeof this[name]}"`, name, true, type, typeof this[name]));
             }
         }
         
