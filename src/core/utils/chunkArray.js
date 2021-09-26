4c82721440c7e9ae57defefdf81ed72be613157d
Callum <callumnewsome100@gmail.com>
2021-09-26 20:53:02 +0100

    chunkArray util

---
+++ src/core/utils/chunkArray.js
@@ -0,0 +1,15 @@
+function chunkArray(inputArray, perChunk = 2) {    
+    return inputArray.reduce((resultArray, item, index) => { 
+        const chunkIndex = Math.floor(index/perChunk)
+        
+        if(!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+        
+        resultArray[chunkIndex].push(item)
+        
+        return resultArray
+    }, [])
+}
+
+module.exports = chunkArray;
