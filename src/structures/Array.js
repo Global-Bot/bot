c49bd80af857ec1a8520a4c22766b6435c0adf6a
Callum <callumnewsome100@gmail.com>
2021-10-25 21:02:47 +0100

    Remove useless brackets

--- src/structures/Array.js
+++ src/structures/Array.js
@@ -1,5 +1,5 @@
 // Array.random - Returns a random index 
 Object.defineProperty(Array.prototype, "random", {
-    get() { return () => this[Math.floor((Math.random() * this.length))] },
+    get() { return () => this[ Math.floor(Math.random() * this.length) ] },
     enumerable: false,
 });
