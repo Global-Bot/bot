8ca0900aa9a8509a49d1086393aac3da006e0af0
Sam Ennis <samennis127@gmail.com>
2021-11-07 01:45:17 +0000

    Added weekly stars tracking to leaderboard

--- src/core/classes/managers/LeaderboardManager.js
+++ src/core/classes/managers/LeaderboardManager.js
@@ -7,10 +7,49 @@ class LeaderboardManager extends Base {
         super(global);
         this.settings = this.config.leaderboardSettings;
         this._guildID = this.config.base_guild;
+        this.topUsers = {};
         this.database = this.models.leaderboard;
         
         setTimeout(() => {this.init();}, 5000)
         setInterval(() => {this.init()}, 60000*5)
+    }
+    
+    async startTimer() {
+        while(true) {
+            let time = this.calculateTime();
+            this.logger.info(`Leaderboard Time: ${time}ms`)
+            await new Promise(res => setTimeout(res, time));
+            for(let type of TYPES) {
+                let {table, order, role} = this.DBS[type]
+                let getLeaderboardData = await this.models[table].findAll({order: [[order, 'DESC']]});
+                let winner = getLeaderboardData[0];
+                
+                let guild = this.client.guilds.cache.get(this._guildID);
+                if(!guild) return;
+                let roleObject = guild.roles.cache.get(role);
+                if(!role) continue;
+                for(let mem of roleObject.members.values()) {
+                    mem.roles.remove(role).catch(() => {});
+                }
+                winner.roles.add(role).catch(() => {});
+            }
+            await this.models.economy.update({weeklyStars: 0})
+        }
+    }
+    
+    calculateTime() {
+        let now = this.moment();
+        let today = now.isoWeekday();
+        let wantDate = this.moment().set({hour: 18, minute: 0, second: 0, millisecond: 0})
+        const dayINeed = 6; // Saturday
+        if (today < dayINeed) { 
+            wantDate = wantDate.isoWeekday(dayINeed)
+        } else if(today > dayINeed || today == dayINeed && now.get("hour") >= 18) {
+            wantDate = wantDate.add(1, 'weeks').isoWeekday(dayINeed);
+        }
+        
+        let distance = wantDate.valueOf() - now.valueOf()
+        return distance;
     }
     
     async init() {
@@ -28,11 +67,11 @@ class LeaderboardManager extends Base {
             
             let {table, order} = this.DBS[type]
             let getLeaderboardData = await this.models[table].findAll({order: [[order, 'DESC']]});
-
+            
             let format = this.format(type, getLeaderboardData, this.DBS[type]);
-
+            
             getMessage.edit({embeds: [format]});
-
+            
             if(getData) {
                 this.database.update({channel_id: channel.id, message_id: getMessage.id}, {where: {type}})
             } else {
@@ -54,23 +93,25 @@ class LeaderboardManager extends Base {
         `At the end of the week, the member top user on this leaderboard will receive <@&${typeData.role}>`,
         ``,
         `Last updated: <t:${Math.round(new Date().getTime()/1000)}:R>\nResets: <t:${Math.round(this.moment().set("day", 6).toDate().getTime()/1000)}:R>`
-        ]
-
-        let finalString = wording.join("\n")
-
-        let embed = new MessageEmbed()
-        .setDescription(finalString)
-        .setThumbnail(this.client.user.avatarURL())
-        .setColor('PURPLE');
-
-        return embed;
-    }
+    ]
     
-    get DBS() {
-        return {
-            "stars": {table: "economy", order: "stars", role: "878760379587653653", emoji: this.config.emojis.get("star")}
-        }
+    this.topUsers[type] = topTen[0] || "";
+    
+    let finalString = wording.join("\n")
+    
+    let embed = new MessageEmbed()
+    .setDescription(finalString)
+    .setThumbnail(this.client.user.avatarURL())
+    .setColor('PURPLE');
+    
+    return embed;
+}
+
+get DBS() {
+    return {
+        "stars": {table: "economy", order: "weeklyStars", role: "878760379587653653", emoji: this.config.emojis.get("star")}
     }
+}
 }
 
 module.exports = LeaderboardManager;
