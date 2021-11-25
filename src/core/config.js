494b32f918d09da6ad4d0b42344d90e031574284
Callum <callumnewsome100@gmail.com>
2021-11-25 00:10:53 +0000

    Add RoleManager module in config

--- src/core/config.js
+++ src/core/config.js
@@ -69,7 +69,8 @@ config.clientOptions = {
         'CommandHandler',
         'ButtonHandler',
         'BoostReaction',
-        'DropHandler'
+        'DropHandler',
+        'RoleManager'
     ]
 }
 
